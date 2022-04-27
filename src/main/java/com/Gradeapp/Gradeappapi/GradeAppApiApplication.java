e00119ff23f168238ecfddc3e285a23410acf5f5
V777-vino <75883365+V777-vino@users.noreply.github.com>
2022-04-27 09:46:52 +0530

    first commit

---
+++ src/main/java/com/Gradeapp/Gradeappapi/GradeAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.Gradeapp.Gradeappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeAppApiApplication.class, args);
+	}
+
+}
